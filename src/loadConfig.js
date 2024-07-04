8b09b351a4bc43ec38bf76ef852fddbaed2c9042
Qianmo <92790790+Qianmoxsn@users.noreply.github.com>
2024-07-04 14:04:00 +0800

    别名

--- src/loadConfig.js
+++ src/loadConfig.js
@@ -1,19 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const yargs = require('yargs');
+const logger = require('./logger');
 
 let config;
 
 const argv = yargs
     .option('ipv4', {
+        alias: 's',
         description: '上游DNS服务器的IPv4地址',
         type: 'string'
     })
     .option('ipv6', {
+        alias: 'ss',
         description: '上游DNS服务器的IPv6地址',
         type: 'string'
     })
     .option('port', {
+        alias: 'p',
         description: '上游DNS服务器的端口号',
         type: 'number'
     })
@@ -21,18 +25,12 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
-const defaultConfigPath = path.resolve(__dirname, 'config.json');
+const defaultConfigPath = path.resolve(__dirname, '../config.json');
 
 if (fs.existsSync(defaultConfigPath)) {
     config = JSON.parse(fs.readFileSync(defaultConfigPath, 'utf8'));
 } else {
-    config = {
-        upstreamDNS: {
-            ipv4: '8.8.8.8',
-            ipv6: '2400:3200::1',
-            port: 53
-        }
-    };
+    logger.warn('Config file not found, using param settings');
 }
 
 config.upstreamDNS.ipv4 = argv.ipv4 || config.upstreamDNS.ipv4;
